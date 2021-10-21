e6d8291d718c799832f6e75dcd591c0cc1703bef
shinigamiishot <shinigamiishot@gmail.com>
2021-10-21 17:05:02 +0200

    Change Text

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 In diesem Repository werden wir ganz viel Python lernen.
 
 Python macht Spaß! :)
+
---
+++ bmi_berechnen.py
@@ -0,0 +1,11 @@
+vorname = input("Wie lautet dein Vorname")
+nachname = input("Wie lautet dein Nachname")
+geschlecht = input("Welches Gechlecht hast du gerade? ")
+groesse = input("Wie groß bist du? ")
+gewicht = input("Wie schwer bist du? ")
+
+satz = f"""
+Liebe/r {vorname} {nachname},
+du hast einen BMI von ... 
+"""
+print(satz)
